8318ed157a4cffe1fd1b0a5e9f46c7c9557719b9
Matthieu Mota <matthieumota@gmail.com>
2019-12-13 10:17:00 +0100

    Add unit test

--- app/code/Boxydev/Slider/Model/Slide.php
+++ app/code/Boxydev/Slider/Model/Slide.php
@@ -14,9 +14,27 @@ namespace Boxydev\Slider\Model;
 use Boxydev\Slider\Api\Data\SlideInterface;
 use Magento\Framework\Model\AbstractModel;
 use Boxydev\Slider\Model\ResourceModel\Slide as SlideResource;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Slide extends AbstractModel implements SlideInterface
 {
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        StoreManagerInterface $storeManager,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+        $this->storeManager = $storeManager;
+    }
+
     protected function _construct()
     {
         $this->_init(SlideResource::class);
@@ -50,5 +68,10 @@ class Slide extends AbstractModel implements SlideInterface
     public function setImage($image)
     {
         return $this->setData('image', $image);
+    }
+
+    public function getImageUrl()
+    {
+        return $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'boxydev/slide/' . $this->getImage();
     }
 }
---
+++ app/code/Boxydev/Slider/Test/Unit/Model/SlideTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the magento.com package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Boxydev\Slider\Test\Unit\Model;
+
+use Boxydev\Slider\Api\Data\SlideInterface;
+use Boxydev\Slider\Model\Slide;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class SlideTest extends TestCase
+{
+    /**
+     * @var Slide
+     */
+    protected $_model;
+
+    protected function setUp()
+    {
+        $storeMock = $this->getMockBuilder(StoreInterface::class)
+            ->setMethods(['getBaseUrl'])
+            ->getMockForAbstractClass();
+        $storeMock->method('getBaseUrl')->willReturn('http://localhost/media/');
+
+        $storeManagerMock = $this->createMock(StoreManagerInterface::class);
+        $storeManagerMock->method('getStore')->willReturn($storeMock);
+
+        $this->_model = (new ObjectManager($this))->getObject(Slide::class, [
+            'storeManager' => $storeManagerMock
+        ]);
+    }
+
+    public function testSlideIsInstanceOfModel()
+    {
+        $this->assertInstanceOf(AbstractModel::class, $this->_model);
+        $this->assertInstanceOf(SlideInterface::class, $this->_model);
+    }
+
+    public function testSlideHasGettersSetters()
+    {
+        $this->assertNull($this->_model->getId());
+        $this->_model->setId(5);
+        $this->assertSame(5, $this->_model->getId());
+    }
+
+    /**
+     * @dataProvider slides
+     */
+    public function testSlideHasAnImageUrl($image, $url)
+    {
+        $this->_model->setImage($image);
+        $this->assertSame($url, $this->_model->getImageUrl());
+    }
+
+    public function slides()
+    {
+        return [
+            ['toto.jpg', 'http://localhost/media/boxydev/slide/toto.jpg'],
+            ['titi.jpg', 'http://localhost/media/boxydev/slide/titi.jpg']
+        ];
+    }
+}
