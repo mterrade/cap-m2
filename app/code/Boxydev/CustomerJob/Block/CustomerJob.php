c5fc6caeb3c60b1c9bfca13d81f6bbd6a3f1d9f9
Matthieu Mota <matthieumota@gmail.com>
2019-12-13 16:29:50 +0100

    Collection customer and repository

--- app/code/Boxydev/CustomerJob/Block/CustomerJob.php
+++ app/code/Boxydev/CustomerJob/Block/CustomerJob.php
@@ -4,6 +4,7 @@ namespace Boxydev\CustomerJob\Block;
 
 use Boxydev\CustomerJob\Model\Attribute\Source\Job;
 use Magento\Customer\Model\Session;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
@@ -19,20 +20,52 @@ class CustomerJob extends Template
      */
     private $job;
 
+    /**
+     * @var \Magento\Customer\Model\ResourceModel\Customer\Collection
+     */
+    private $customerCollection;
+
+    /**
+     * @var \Magento\Customer\Api\CustomerRepositoryInterface
+     */
+    private $customerRepository;
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
     public function __construct(
         Context $context,
         Session $customerSession,
         Job $job,
+        \Magento\Customer\Model\ResourceModel\Customer\Collection $customerCollection,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->customerSession = $customerSession;
         $this->job = $job;
+        $this->customerCollection = $customerCollection;
+        $this->customerRepository = $customerRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
     }
 
     public function getCustomer()
     {
         return $this->customerSession->getCustomer();
+    }
+
+    public function getCustomerCollection()
+    {
+        return $this->customerCollection;
+    }
+
+    public function getCustomerListByRepository()
+    {
+        return $this->customerRepository->getList(
+            $this->searchCriteriaBuilder->create()
+        );
     }
 
     public function getJobs()
